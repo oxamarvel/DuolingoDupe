6cb8edf19a91e2b4962df3a2e8c23349e2a9debc
oxa <oxamarvel@gmail.com>
2025-04-17 13:00:31 +0700

    Initial Commit

---
+++ DuolingoDupe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DuolingoDupe
+//
+//  Created by Oxa Marvel on 17/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DuolingoDupe/DuolingoDupeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuolingoDupeApp.swift
+//  DuolingoDupe
+//
+//  Created by Oxa Marvel on 17/04/25.
+//
+
+import SwiftUI
+
+@main
+struct DuolingoDupeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
